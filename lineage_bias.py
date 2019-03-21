6f7d8bd805fde7ecc2aaccadeb6f83caa98bdcef
Samir Akre <akre@usc.edu>
2019-03-20 17:32:24 -0700

    finished adding preliminary tests for lineage bias processing verification

--- lineage_bias.py
+++ lineage_bias.py
@@ -7,6 +7,11 @@ import pandas as pd
 from aggregate_functions import filter_threshold
 
 def calc_angle(gr: float, b: float) -> float:
+    if b == 0.0:
+        if gr == 0.0:
+            return pi/4
+        return pi/2
+
     towards_myloid_angle = atan(gr/b)
     return towards_myloid_angle
 
@@ -17,9 +22,17 @@ def calc_bias_from_angle(theta: float) -> float:
 def calc_bias(gr: float, b: float) -> float:
     return calc_bias_from_angle(calc_angle(gr, b))
 
-def calculate_baseline(input_df: pd.DataFrame, baseline_timepoint: int, baseline_column: str = 'month') -> pd.DataFrame:
+def calculate_baseline(input_df: pd.DataFrame, baseline_timepoint: int = 4, baseline_column: str = 'month') -> pd.DataFrame:
     # Filter by timepoint
     timepoint_df = input_df.loc[input_df[baseline_column] == baseline_timepoint]
+    baseline_months_df = pd.DataFrame(input_df.groupby(['mouse_id', 'cell_type'])[baseline_column].min()).reset_index()
+
+    if not baseline_months_df[baseline_months_df.month != baseline_timepoint].empty:
+        print(baseline_months_df[baseline_months_df.month != 4].empty)
+        print('Error: No baseline value establishable')
+        print('The following data lacks baseline information:')
+        print(baseline_months_df[baseline_months_df.month != baseline_timepoint])
+        raise ValueError('Error: No baseline value establishable')
 
     # Find max
     baseline_max_df = pd.DataFrame(timepoint_df.groupby(['mouse_id', 'cell_type']).percent_engraftment.max()).reset_index()
@@ -62,39 +75,44 @@ def create_lineage_bias_df(norm_data_df: pd.DataFrame) -> pd.DataFrame:
     lineage_bias_df = pd.DataFrame(columns=lineage_bias_columns)
 
     for _, group in norm_data_df.groupby(['mouse_id', 'code', 'day']):
-        has_both_types = True
-        for cell_type in ['gr', 'b']:
-            if group[group.cell_type == cell_type].empty:
-                has_both_types = False
 
-        if len(group) < 2:
-            has_both_types = False
+        if group[group.cell_type == 'gr'].empty:
+            gr_value = 0.0
+        else:
+            gr_value = group[group.cell_type == 'gr'].norm_percent_engraftment.values[0]
 
-        if has_both_types:
-            new_row = pd.DataFrame(columns=lineage_bias_columns)
-            gr = group[group.cell_type == 'gr'].norm_percent_engraftment.values[0]
-            b = group[group.cell_type == 'b'].norm_percent_engraftment.values[0]
-            new_row['has_null'] = [group.norm_percent_engraftment.isnull().values.any()]
-            new_row['lineage_bias'] = [calc_bias(gr, b)]
-            new_row['code'] = group.code.unique()
-            new_row['user'] = group.user.unique()
-            new_row['day'] = group.day.unique()
-            new_row['month'] = group.month.unique()
-            new_row['mouse_id'] = group.mouse_id.unique()
-            new_row['group'] = group['group'].unique()
-            if(len(new_row.code) > 1):
-                print('ERROR MULTIPLE CODES FOUND FOR GROUP')
-                sys.exit(1)
-            lineage_bias_df = lineage_bias_df.append(new_row, sort=False, ignore_index=True)
+        if group[group.cell_type == 'b'].empty:
+            b_value = 0.0
+        else:
+            b_value = group[group.cell_type == 'b'].norm_percent_engraftment.values[0]
+
+        if len(group) > 3:
+            raise ValueError('More cell types than expected')
+
+        new_row = pd.DataFrame(columns=lineage_bias_columns)
+        new_row['has_null'] = [group.norm_percent_engraftment.isnull().values.any()]
+        new_row['lineage_bias'] = [calc_bias(gr_value, b_value)]
+        new_row['code'] = group.code.unique()
+        new_row['user'] = group.user.unique()
+        new_row['day'] = group.day.unique()
+        new_row['month'] = group.month.unique()
+        new_row['mouse_id'] = group.mouse_id.unique()
+        new_row['group'] = group['group'].unique()
+
+        if(len(new_row.code) > 1):
+            raise ValueError('Multiple codes found for group')
+
+        lineage_bias_df = lineage_bias_df.append(new_row, sort=False, ignore_index=True)
     return lineage_bias_df
 
 def main():
     input_df = pd.read_csv('Ania_M_all_percent-engraftment_100818_long.csv')
+    present_df = filter_threshold(input_df, .01, ['gr', 'b'])
 
     norm_data_df = normalize_input(input_df)
     lineage_bias_df = create_lineage_bias_df(norm_data_df)
     print(lineage_bias_df)
-    #lineage_bias_df.to_csv('lineage_bias.csv', index=False)
+    lineage_bias_df.to_csv('lineage_bias.csv', index=False)
             
 
 if __name__ == '__main__':
--- test/test_lineage_bias.py
+++ test/test_lineage_bias.py
@@ -1,9 +1,10 @@
 """ Test lineage bias calculation scripts
+
 """
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal
 from ..lineage_bias import create_lineage_bias_df, calculate_baseline, calc_bias, normalize_to_baseline, normalize_input
 
 TEST_LONG_DATA = pd.read_csv('test/test_data/test_long_for_lineage_bias.csv')
@@ -33,25 +34,96 @@ def test_norm_engraftment_change():
     assert_array_equal(test_data.sort_values(by=['cell_type', 'norm_percent_engraftment_expected']).norm_percent_engraftment_expected, norm_data_df.sort_values(by=['cell_type', 'norm_percent_engraftment']).norm_percent_engraftment)
 
 def test_baseline_missing_value():
-    test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M4']
-    baseline_df = calculate_baseline(test_data, baseline_timepoint=4, baseline_column='month')
-    print(baseline_df)
+    """ Test that if missing value at baseline, throws ValueError
+    """
 
-    # Currently keeps nan's if no value
-    assert_array_equal(baseline_df.baseline_max, baseline_df.baseline_max_expected)
-
-def test_norm_missing_value():
     test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M4']
+    try:
+        calculate_baseline(test_data, baseline_timepoint=4, baseline_column='month')
+        assert False
+    except ValueError:
+        assert True
+
+def test_norm_missing_baseline_value():
+    """ Test normalized value, if value missing
+
+    Currently we expect the normalized baseline engraftment to be NaN
+    if the value is missing at 4 month, but since no values like that
+    exist in current dataset, throws value error.
+    """
+
+    test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M4']
+    try:
+        normalize_input(test_data, baseline_timepoint=4, baseline_column='month', analyzed_cell_types=['gr', 'b'])
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_baseline_set_max_mouse():
+    """ Test that baseline values are set using the max from all clones for a mouse&day&cell_type
+
+    Test data has two clones for mouse M5 in 2 timepoints
+    """
+    test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M5']
     norm_data_df = normalize_input(test_data, baseline_timepoint=4, baseline_column='month', analyzed_cell_types=['gr', 'b'])
-    print(norm_data_df)
 
-    # Norm percent engraftment set to nan
+    assert_array_equal(norm_data_df.baseline_max, norm_data_df.baseline_max_expected)
     assert_array_equal(norm_data_df.norm_percent_engraftment, norm_data_df.norm_percent_engraftment_expected)
 
 def test_lineage_bias_0():
+    """ Test lineage bias for balanced full data
+
+    Expect the lineage bias value to be 0
+    """
+
     test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M1']
     test_expected = EXPECTED_LINEAGE_BIAS.loc[EXPECTED_LINEAGE_BIAS.mouse_id == 'M1']
 
     norm_data_df = normalize_input(test_data, baseline_timepoint=4, baseline_column='month', analyzed_cell_types=['gr', 'b'])
     lineage_bias_df = create_lineage_bias_df(norm_data_df)
     assert_array_equal(lineage_bias_df.lineage_bias, test_expected.lineage_bias_expected)
+
+def test_lineage_bias_change():
+    """ Test Lineage bias that should change over time
+
+    Test done on data with complete values all around
+    """
+
+    test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M3']
+    test_expected = EXPECTED_LINEAGE_BIAS.loc[EXPECTED_LINEAGE_BIAS.mouse_id == 'M3']
+
+    norm_data_df = normalize_input(test_data, baseline_timepoint=4, baseline_column='month', analyzed_cell_types=['gr', 'b'])
+    lineage_bias_df = create_lineage_bias_df(norm_data_df)
+    assert_array_almost_equal(lineage_bias_df.lineage_bias, test_expected.lineage_bias_expected, decimal=6)
+
+def test_baseline_gen_nobaseline():
+    """ Test baseline calculation if there is no baseline for an entry
+
+    This doesn't happen, expect a valueError
+
+    If there is no 4month baseline value to normalize to,
+    normalize to max at current timepoint for mouse, propogate
+    that timepoint for any later values.
+    """
+
+    test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M4']
+    try:
+        normalize_input(test_data, baseline_timepoint=4, baseline_column='month', analyzed_cell_types=['gr', 'b'])
+        assert False
+    except ValueError:
+        assert True
+
+
+def test_lineage_bias_no_gr():
+    """ Test behaviour if no gr value at non-baseline forming month
+
+    """
+    test_data = TEST_LONG_DATA.loc[TEST_LONG_DATA.mouse_id == 'M6']
+    test_expected = EXPECTED_LINEAGE_BIAS.loc[EXPECTED_LINEAGE_BIAS.mouse_id == 'M6']
+    
+    norm_data_df = normalize_input(test_data, baseline_timepoint=4, baseline_column='month', analyzed_cell_types=['gr', 'b'])
+    lineage_bias_df = create_lineage_bias_df(norm_data_df)
+    assert_array_almost_equal(lineage_bias_df.lineage_bias, test_expected.lineage_bias_expected, decimal=6)
+
+
