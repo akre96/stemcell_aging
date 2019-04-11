065bfd54cac36231b55aa5604d6731efc7c503da
Samir Akre <akre@usc.edu>
2019-04-11 16:23:30 -0700

    :construction: WIP started looking at clustering

---
+++ cluster_clones.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from sklearn.cluster import affinity_propagation
+
+
+train = pd.read_csv('~/Data/stemcell_aging/aging_proc_03122019_long/Ania_M3000_percent-engraftment_100818_long.csv')
