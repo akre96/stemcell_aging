b1bb3cf0acbebbc7ff995367d2907195e22c0a0a
Samir Akre <akre@usc.edu>
2019-05-01 11:46:26 -0700

    :hammer: refactored to work with serial transplant as well

--- cluster_clones.py
+++ cluster_clones.py
@@ -12,28 +12,41 @@ from plotting_functions import save_plot
 
 save=True
 COLOR_PALETTES = json.load(open('color_palettes.json', 'r'))
-train = pd.read_csv('~/Data/stemcell_aging/lineage_bias/lineage_bias_t0-0_from-counts.csv')
-data = train[train.group.isin(['aging_phenotype','no_change'])]
-train = train[train.group.isin(['aging_phenotype','no_change'])]
-
 categorical_cols = ['mouse_id', 'group']
+categorical_cols = []
 numerical_cols = ['gr_percent_engraftment', 'b_percent_engraftment', 'lineage_bias']
-numerical_cols = ['lineage_bias']
+numerical_cols = [numerical_cols[1]]
 
 initials = [x[:3] for x in numerical_cols]
 save_folder = '_'.join(initials)
-save_path = '/home/sakre/Data/stemcell_aging/Graphs/Cluster_Exploration/'+save_folder
+
+# Uncomment for Aging Data
+#train = pd.read_csv('~/Data/stemcell_aging/lineage_bias/lineage_bias_t0-0_from-counts.csv')
+#timepoint_col='month'
+#save_path = '/home/sakre/Data/stemcell_aging/Graphs/Cluster_Exploration/'+save_folder
+
+# Uncomment for Serial Transplant Data
+train = pd.read_csv('~/Data/serial_transplant_data/lineage_bias/lineage_bias_t0-0_from-counts.csv')
+timepoint_col='day'
+save_path = '/home/sakre/Data/serial_transplant_data/Graphs/Cluster_Exploration/'+save_folder
+
+data = train[train.group.isin(['aging_phenotype','no_change'])]
+train = train[train.group.isin(['aging_phenotype','no_change'])]
+
 #scaler = StandardScaler()
 #train[numerical_cols] = scaler.fit_transform(train[numerical_cols])
-
-encoded = pd.get_dummies(train[categorical_cols+numerical_cols])
+#encoded = pd.get_dummies(train[categorical_cols+numerical_cols])
 
 # Transform to shape [code, time, nfeatures]
-X_train = np.zeros((len(train[['code', 'mouse_id']].drop_duplicates()), 4, 3))
+X_train = np.zeros(
+    (len(train[['code', 'mouse_id']].drop_duplicates()),
+    train[timepoint_col].nunique(),
+    len(numerical_cols) + len(categorical_cols))
+)
 i = 0
 for _, clone_group in train.groupby(['code', 'mouse_id']):
     j = 0
-    for _, time_group in clone_group.groupby('month'):
+    for _, time_group in clone_group.groupby(timepoint_col):
         X_train[i][j] = time_group[numerical_cols].to_numpy()
         j = j + 1
     i = i+1
@@ -50,8 +63,12 @@ y_pred = km.fit_predict(X_train)
 with_label_df = pd.DataFrame()
 for _, clone_group in data.groupby(['code', 'mouse_id']):
     j = 0
-    clone_test = np.zeros((1, 4, 3))
-    for _, time_group in clone_group.groupby('month'):
+    clone_test = np.zeros((
+        1,
+        train[timepoint_col].nunique(),
+        len(numerical_cols) + len(categorical_cols)
+    ))
+    for _, time_group in clone_group.groupby(timepoint_col):
         clone_test[0][j] = time_group[numerical_cols].to_numpy()
         j = j + 1
     
@@ -62,37 +79,37 @@ for _, clone_group in data.groupby(['code', 'mouse_id']):
     with_label_df = with_label_df.append(clone_group)
 
 lb_mean_data_df = pd.DataFrame(
-    with_label_df.groupby(['mouse_id','month','label', 'group']).lineage_bias.mean()
+    with_label_df.groupby(['mouse_id',timepoint_col,'label', 'group']).lineage_bias.mean()
 ).reset_index().rename(columns={'lineage_bias': 'Average Lineage Bias'})
 b_sum_data_df = pd.DataFrame(
-    with_label_df.groupby(['mouse_id','month','label', 'group']).b_percent_engraftment.sum()
+    with_label_df.groupby(['mouse_id',timepoint_col,'label', 'group']).b_percent_engraftment.sum()
 ).reset_index().rename(columns={'b_percent_engraftment':'Sum B Abundance'})
 gr_sum_data_df = pd.DataFrame(
-    with_label_df.groupby(['mouse_id','month','label', 'group']).gr_percent_engraftment.sum()
+    with_label_df.groupby(['mouse_id',timepoint_col,'label', 'group']).gr_percent_engraftment.sum()
 ).reset_index().rename(columns={'gr_percent_engraftment':'Sum Gr Abundance'})
 b_mean_data_df = pd.DataFrame(
-    with_label_df.groupby(['mouse_id','month','label', 'group']).b_percent_engraftment.mean()
+    with_label_df.groupby(['mouse_id',timepoint_col,'label', 'group']).b_percent_engraftment.mean()
 ).reset_index().rename(columns={'b_percent_engraftment':'Average B Abundance'})
 gr_mean_data_df = pd.DataFrame(
-    with_label_df.groupby(['mouse_id','month','label', 'group']).gr_percent_engraftment.mean()
+    with_label_df.groupby(['mouse_id',timepoint_col,'label', 'group']).gr_percent_engraftment.mean()
 ).reset_index().rename(columns={'gr_percent_engraftment':'Average Gr Abundance'})
-sum_data_df = lb_mean_data_df.merge(b_sum_data_df, on=['mouse_id', 'month', 'label', 'group'])
-sum_data_df = sum_data_df.merge(gr_sum_data_df, on=['mouse_id', 'month', 'label', 'group'])
-sum_data_df = sum_data_df.merge(gr_mean_data_df, on=['mouse_id', 'month', 'label', 'group'])
-sum_data_df = sum_data_df.merge(b_mean_data_df, on=['mouse_id', 'month', 'label', 'group'])
+sum_data_df = lb_mean_data_df.merge(b_sum_data_df, on=['mouse_id', timepoint_col, 'label', 'group'])
+sum_data_df = sum_data_df.merge(gr_sum_data_df, on=['mouse_id', timepoint_col, 'label', 'group'])
+sum_data_df = sum_data_df.merge(gr_mean_data_df, on=['mouse_id', timepoint_col, 'label', 'group'])
+sum_data_df = sum_data_df.merge(b_mean_data_df, on=['mouse_id', timepoint_col, 'label', 'group'])
 
 
 for y_col in ['Sum B Abundance', 'Sum Gr Abundance', 'Average Gr Abundance', 'Average B Abundance', 'Average Lineage Bias']:
     plt.figure(figsize=(8, 14))
     sns.lineplot(
-        x='month',
+        x=timepoint_col,
         y=y_col,
         data=sum_data_df,
         hue='label',
         palette=COLOR_PALETTES['cluster_label'],
         style='group',
-        err_style='bars',
-        linewidth=2,
+        err_style=None,
+        linewidth=4,
     )
     plt.title(y_col)
     fname = save_path + os.sep + y_col.lower().replace(' ', '_') + '.png'
@@ -102,10 +119,13 @@ for name, cluster in with_label_df.groupby(['group', 'label']):
     print(
         'Group: ' \
         + name[0] \
-        + ' Label: ' \
+        + ', Label: ' \
         + name[1] \
-        + ' Unique Clones: ' \
-        + str(cluster.code.nunique())
+        + ', Unique Mice: ' \
+        + str(cluster.mouse_id.nunique()) \
+        + ', Unique Clones: ' \
+        + str(cluster.code.nunique()) \
+
     )
 if not save:
     plt.show()
