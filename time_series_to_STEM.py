f951f3f0547bd4e8b214d4776d78a0e474e2b5ad
Samir Akre <akre@usc.edu>
2019-06-04 09:58:46 -0700

    :sparkles: added filtering for abundance greater than threshold  at any time point for STEM input data

--- time_series_to_STEM.py
+++ time_series_to_STEM.py
@@ -1,11 +1,24 @@
 import pandas as pd
+root_dir = '~/Data/serial_transplant_data'
 data = pd.read_csv('~/Data/serial_transplant_data/M_allAniaAnia serial transpl_percent-engraftment_121018_long.csv')
+root_dir = '~/Data/stemcell_aging'
+data = pd.read_csv('~/Data/stemcell_aging/Ania_M_allAnia_percent-engraftment_052219_long.csv')
 
-gr_data = data[data.cell_type == 'b']
-piv = gr_data.pivot_table(
+onep_anywhere = data[data.percent_engraftment > .01]
+onep_anywhere = onep_anywhere[['code', 'mouse_id']].drop_duplicates()
+filt_data = data.merge(
+    onep_anywhere,
+    how='inner',
+    on=['code', 'mouse_id'],
+    validate='m:1'
+)
+
+
+cell_type = 'gr'
+cell_data = filt_data[filt_data.cell_type == cell_type]
+piv = cell_data.pivot_table(
     index=['code'],
     columns=['day'],
     values='percent_engraftment'
 )
-
-piv.to_csv('~/Data/serial_transplant_data/STEM_Clustering/b_data.tsv', sep='\t')
+piv.to_csv(root_dir + '/STEM_Clustering/'+ cell_type +'_data_filt-0-01p-anywhere.tsv', sep='\t')
