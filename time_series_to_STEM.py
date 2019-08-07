5184d083f6ca590252a048f8961e94ebe82b662a
Samir Akre <akre@usc.edu>
2019-08-07 15:08:15 -0700

    :sparkles: reorganized STEM data creation, does all 4 combinations of group/cell_type at once now

--- time_series_to_STEM.py
+++ time_series_to_STEM.py
@@ -1,20 +1,16 @@
+import os
 import pandas as pd
 import numpy as np
 
-root_dir = '~/Data/serial_transplant_data'
-data = pd.read_csv('~/Data/serial_transplant_data/M_allAniaAnia serial transpl_percent-engraftment_121018_long.csv')
+#root_dir = '~/Data/serial_transplant_data'
+#data = pd.read_csv('~/Data/serial_transplant_data/M_allAniaAnia serial transpl_percent-engraftment_121018_long.csv')
 
-root_dir = '~/Data/stemcell_aging'
+root_dir = '/home/sakre/Data/stemcell_aging'
 data = pd.read_csv('~/Data/stemcell_aging/Ania_M_allAnia_percent-engraftment_052219_long.csv')
 
-cell_type = 'gr'
-group = 'aging_phenotype'
 p_anywhere_filter = 0.01
-change_p = 0.01
+change_p = 0.1
 
-
-if group != 'all':
-    data = data[data.group == group]
 
 onep_anywhere = data[data.percent_engraftment > p_anywhere_filter]
 onep_anywhere = onep_anywhere[['code', 'mouse_id']].drop_duplicates()
@@ -24,37 +20,54 @@ filt_data = data.merge(
     on=['code', 'mouse_id'],
     validate='m:1'
 )
-cell_data = filt_data[filt_data.cell_type == cell_type]
+for cell_type in ['gr', 'b']:
+    for group in ['aging_phenotype', 'no_change']:
+        group_data = filt_data[filt_data.group == group]
+        cell_data = group_data[group_data.cell_type == cell_type]
 
-## Make change relative to first >= change_p in abundance
-first_tp = cell_data[cell_data.day == cell_data['day'].min()].rename(columns={'percent_engraftment': 'first_abund'})[['code', 'mouse_id', 'first_abund']]
-not_first_tp = cell_data[cell_data.day != cell_data['day'].min()]
-changes_codes = []
-for time, tp_df in not_first_tp.groupby('day'):
-    t_diff_df = first_tp.merge(
-        tp_df,
-        how='inner',
-        validate='1:1'
-    )
-    pass_filt = t_diff_df[np.abs(t_diff_df.percent_engraftment - t_diff_df.first_abund) > change_p]
-    changes_codes = changes_codes + pass_filt.code.unique().tolist()
+        ## Make change relative to first >= change_p in abundance
+        first_tp = cell_data[cell_data.day == cell_data['day'].min()].rename(columns={'percent_engraftment': 'first_abund'})[['code', 'mouse_id', 'first_abund']]
+        not_first_tp = cell_data[cell_data.day != cell_data['day'].min()]
+        changes_codes = []
+        for time, tp_df in not_first_tp.groupby('day'):
+            t_diff_df = first_tp.merge(
+                tp_df,
+                how='inner',
+                validate='1:1'
+            )
+            pass_filt = t_diff_df[np.abs(t_diff_df.percent_engraftment - t_diff_df.first_abund) >= change_p]
+            changes_codes = changes_codes + pass_filt.code.unique().tolist()
 
-filt_change_df = cell_data[cell_data.code.isin(changes_codes)]
+        filt_change_df = cell_data[cell_data.code.isin(changes_codes)]
 
 
 
-piv = filt_change_df.pivot_table(
-    index=['code'],
-    columns=['day'],
-    values='percent_engraftment'
-)
-piv.to_csv(
-    root_dir 
-    + '/STEM_Clustering/'
-    + cell_type 
-    + '_' + group
-    + '_data_filt_' 
-    + str(p_anywhere_filter).replace('.', '-')
-    + '_change_'
-    + str(change_p).replace('.', '-')
-    + '_anywhere.tsv', sep='\t')
+        piv = filt_change_df.pivot_table(
+            index=['code'],
+            columns=['day'],
+            values='percent_engraftment'
+        )
+        save_dir = \
+            root_dir \
+            + '/STEM_Clustering/' \
+            + 'data_filt_' \
+            + str(p_anywhere_filter).replace('.', '-') \
+            + '_change_' \
+            + str(change_p).replace('.', '-') 
+
+        if not os.path.exists(save_dir):
+            print('Creating', save_dir)
+            os.makedirs(save_dir)
+
+        piv.to_csv(
+            save_dir
+            + '/'
+            + cell_type
+            + '_' + group
+            + '_data_filt_'
+            + str(p_anywhere_filter).replace('.', '-')
+            + '_change_'
+            + str(change_p).replace('.', '-')
+            + '.tsv',
+            sep='\t'
+            )
